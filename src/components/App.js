931ba221223a07cbbca3c74ceb075fe469692521
wesley <wesleyyw8@gmail.com>
2018-08-18 00:05:37 +0100

    initial structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        asdasdasdasdasd
+      </div>
+    );
+  }
+}
+
+export default connect()(App);

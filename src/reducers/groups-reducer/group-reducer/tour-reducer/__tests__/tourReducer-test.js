eb71ebd2212924bff53eeca9c1fdf9aec01046f1
wesley <wesleyyw8@gmail.com>
2018-09-10 22:56:35 +0100

    tests passing

--- src/reducers/groups-reducer/group-reducer/tour-reducer/__tests__/tourReducer-test.js
+++ src/reducers/groups-reducer/group-reducer/tour-reducer/__tests__/tourReducer-test.js
@@ -4,13 +4,13 @@ import { REDUCE_SEATS_LEFT } from 'Actions/actionTypes';
 describe('tours reducer', () => {
   it('reduces the quantity of seats', () => {
     const state = {
-      seatsLeft: 2
+      seatsLeft: 4
     };
     const action = {
       type: REDUCE_SEATS_LEFT
     };
     expect(tourReducer(state, action)).toEqual({
-      seatsLeft: 4
+      seatsLeft: 3
     });
   });
 });
